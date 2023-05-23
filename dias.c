517992cf42f672187d968f583dd48c35e1dcca96
MRGonorrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2023-05-23 12:44:26 -0500

    Add files via upload

---
+++ dias.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+void main ()
+{
+        int dia=0;
+        printf ("ingrese un numero");
+        scanf("%d",&dia);
+
+        switch(dia)
+        {
+                case 1:printf("lunes");
+                break;
+                 
+                default:printf("nmms we");
+        }
+}
