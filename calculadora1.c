92fd3ad1873ad63deecea98bb9b7f07c80ec670b
MRGonorrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2023-05-23 08:13:54 -0500

    Add files via upload

---
+++ calculadora1.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+int main ()
+{
+
+    int a,b,c ;
+
+    printf("ingrese numero pls");
+    scanf("%d", &a);
+
+    printf("ingrese numero pls");
+    scanf("%d", &b);
+
+    c=a+b;
+
+    printf("su resultado es %d",c);
+
+}
+ 
+
+ 
+    
+
+    
+
+
+
+
+
+
+
---
+++ mayormenor.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+void main ()
+{
+        int a=0,b=0;
+        printf ("ingrese un valor lady");
+        scanf("%d", &a);
+
+         printf ("ingrese un valor lady");
+        scanf("%d", &b);
+
+        if (a>b)
+        {
+
+           printf ("el mayor es %d",a);
+
+        }
+         
+        if (a<b)
+        {
+
+           printf ("el mayor es %d",b);
+
+        }
+             if
+
+             {
+              printf("ambos son iguales");
+             }
+      
+}
