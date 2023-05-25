22e6ecb018a6487f4743563651fcd7b3b726a1ec
MRGonorrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2023-05-24 19:31:02 -0500

    Add files via upload

---
+++ simbolo1.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+void main ()
+{
+        int i=0;
+        printf ("pon un numero del 1 al 5");
+        scanf("%d",&i);
+        if (i<=0)
+        {
+            printf("nmms we");
+            exit(0);
+        }
+        if(i>5)
+        {
+           printf("enserio cabron? ");
+           exit(0);
+        }
+        
+        for (; i < 5; i++);
+        {
+            if(i=1)
+            {
+                printf(" + ");
+            }
+             if(i=2)
+            {
+                printf(" ++ ");
+            }
+             if(i=3)
+            {
+                printf(" +++ ");
+            }
+             if(i=4)
+            {
+                printf(" ++++ ");
+            }
+             if(i=5)
+            {
+                printf(" +++++ ");
+            }
+        }
+       } 
