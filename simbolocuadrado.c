37d2792e61e0a8bcf6c424f8ff68148ac9edf64b
MRGonorrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2023-05-24 19:57:02 -0500

    Add files via upload

---
+++ simbolocuadrado.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+void main ()
+{
+        int i=0;
+        printf ("pon un numero del 1 al 3");
+        scanf("%d",&i);
+        if (i<=0)
+        {
+            printf("nmms we");
+            exit(0);
+        }
+        if(i>3)
+        {
+           printf("enserio cabron? ");
+           exit(0);
+        }
+        
+        for (; i < 5; i++);
+        {
+            if(i=1)
+            {
+                printf(" ⬛⬛⬛ ");
+            }
+             if(i=2)
+            {
+                printf(" \n ⬛⬛⬛ ");
+            }
+             if(i=3)
+            {
+                printf("\n ⬛⬛⬛ ");
+            }
+            
+       } 
+       } 
---
+++ simbolosigno.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+void main ()
+{
+        int i=0;
+        printf ("pon un numero del 1 al 3");
+        scanf("%d",&i);
+        if (i<=0)
+        {
+            printf("nmms we");
+            exit(0);
+        }
+        if(i>3)
+        {
+           printf("enserio cabron? ");
+           exit(0);
+        }
+        
+        for (; i < 5; i++);
+        {
+            if(i=1)
+            {
+                printf(" + - + ");
+                 printf(" \n -   - ");
+                   printf("\n - + - ");
+           
+       } 
+       } 
+       }
