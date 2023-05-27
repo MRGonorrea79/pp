30a7546d6f7104e780255a3e8510719b85f51486
MRGonorrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2023-05-26 21:15:24 -0500

    Add files via upload
    este es el deber cabrones :3

---
+++ simbolocuadrado69.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+void main ()
+{
+        int i=0,n=4;
+        
+       
+        
+        if(i>4)
+        {
+           printf("enserio cabron? ");
+           exit(0);
+        }
+        
+        for (; i < n; i++);
+        {
+            if(i=1)
+            {
+                printf("  ___ ");
+            }
+             if(i=2)
+            {
+                printf(" \n |   | ");
+            }
+            if(i=3)
+            {
+                printf(" \n |___| ");
+            }
+            
+            
+       } 
+       } 
